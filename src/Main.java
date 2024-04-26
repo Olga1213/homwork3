fc55ab3a77fb28df51717c4243832b5891dfec9c
Ольга1213 <O_Mischenkova@mail.ru>
2024-04-26 12:46:08 +0500

    сделала работу № 3

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,40 @@
 public class Main {
+    public static void main(String[] args) {
+        int a = 500;
+        System.out.println(a);
+        byte b = 5;
+        System.out.println(b);
+        short c = -10;
+        System.out.println(c);
+        long d = 5000;
+        System.out.println(d);
+        float e = -2.2f;
+        System.out.println(e);
+        double f = 2.2;
+        System.out.println(f);
 
-int a =500;
-System.out.println("500")
-byte b=5;
-System.out.println("5")
-short c=-10;
-System.out.println("-10")
-long d=5000;
-System.out.println("5000")
-float e=-2.2;
-System.out.println("-2.2")
-double f=2.2;
-System.out.printLn("2.2")
+        float a2 = 27.12f;
+        long b2 = 987678965549l;
+        short c2 = 569;
+        short d2 = -159;
+        short t = 27897;
+        byte r = 67;
 
+        byte studentAP = 23;
+        byte studentAC = 27;
+        byte studentEA = 30;
+        int totalP = 480;
+        int student = totalP / (studentAP + studentAC + studentEA);
+        System.out.println("На каждого ученика рассчитано" + student + "листов бумаги");
 
-float =27.12;
-long = 987 678 965 549;
-short =569;
-short= -159;
-short=27897;
-byte=67;
-
-
-byte studentAP=23;
-byte studentAC=27;
-byte studentEA=30;
-short student=("studentAP+studentAC+studentEA"=+)/480;
-System.out.println ("На каждого ученика рассчитано"=+byte student" листов бумаги")
-
-float machinePerformance=16/2;
-System.out.println ("За 20 минут машина произвела"+=machinePerformance=16/2*20 штук бутылок)
-            System.out.println ("За сутки машина произвела"+=machinePerformance=16/2*24*60 штук бутылок)
-            System.out.println ("За 20 минут машина произвела"+=machinePerformance=16/2*24*60*3штук бутылок)
-            System.out.println ("За 20 минут машина произвела"+=machinePerformance=16/2*24*60*30штук бутылок)
-
-    byte a=120;
-byte b=2;
-byte c=4;
-System.out.println ("В школе, где'=+(a/(b+c)классов нужно="+(a/(b+c)*b банок белой краски и='+(a/(b+c)*c' банок коричневой краски")
+        float machinePerformance = 16 / 2;
+        System.out.println("За 20 минут машина произвела" + machinePerformance * 20 + "штук бутылок");
+        System.out.println("За сутки машина произвела" + machinePerformance * 60 + "штук бутылок");
+        System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 3 + "штук бутылок");
+        System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 30 + "штук бутылок");
 
 
-    double banana=5*0.08;
-double milk=100*0.105*2;
-double iceCreamSundae =2*0.100;
-double rawEggs = 4*0.070;
-    System.out.println ("banana+milk+iceCreamSundae+rawEggs="+ result)*1000
-
-double a=7.0;
-    double b=0.250;
-    double c=0.500;
-    System.out.println ('a/b+a/c')
-
-    double M=67760*10/100;
-    double D=83690*10/100;
-    double K= 76230*10/100;
-    System.out.println ('M+D+K')*12
-    System.out.println ('67760+83690+76230')*12
-    System.out.println ('('M+D+K')*12'-('67760+83690+76230')*12'')
-
-
-
-
-
-
-
+    }
 }
 
 
-
-=
