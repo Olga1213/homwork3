f848037cab5e085e3f0bb2f85634a0d04010d6bb
Ольга1213 <O_Mischenkova@mail.ru>
2024-04-27 22:20:07 +0500

    сделала работу № 3

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,30 @@ public class Main {
         System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 3 + "штук бутылок");
         System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 30 + "штук бутылок");
 
+        byte a3 = 120;
+        byte b3 = 2;
+        byte c3 = 4;
+        System.out.println("В школе, где'+(a3/(b3+c3)классов нужно" + (a3 / 6 * b3 + "банок белой краски и") + (a3 / 6 * c3
+                + "банок коричневой краски");
 
+        double banana = 5 * 0.08;
+        double milk = 100 * 0.105 * 2;
+        double iceCreamSundae = 2 * 0.100;
+        double rawEggs = 4 * 0.070;
+        System.out.println("banana" + "milk" + "iceCreamSundae" + "rawEggs") * 1000);
+
+        double a7 = 7.0;
+        double b7 = 0.250;
+        double c7 = 0.500;
+        System.out.println(a7 / b7 + a7 / c7);
+
+
+        double M = 67760 * 10 / 100;
+        double D = 83690 * 10 / 100;
+        double K = 76230 * 10 / 100;
+        System.out.println("M" + "D" + "K") * 12;
+        System.out.println('67760+83690+76230') * 12;
+        System.out.println('("M"+"D"+"K"') * 12 -(67760+83690+76230)*12;
     }
 }
 
