54ada773cde210ffa030cb826d0e5f017426f0ba
Ольга1213 <O_Mischenkova@mail.ru>
2024-05-03 23:15:40 +0500

    сделала работу № 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //1
         int a = 500;
         System.out.println(a);
         byte b = 5;
@@ -13,6 +14,7 @@ public class Main {
         double f = 2.2;
         System.out.println(f);
 
+        //2
         float a2 = 27.12f;
         long b2 = 987678965549l;
         short c2 = 569;
@@ -20,6 +22,7 @@ public class Main {
         short t = 27897;
         byte r = 67;
 
+        //3
         byte studentAP = 23;
         byte studentAC = 27;
         byte studentEA = 30;
@@ -27,36 +30,43 @@ public class Main {
         int student = totalP / (studentAP + studentAC + studentEA);
         System.out.println("На каждого ученика рассчитано" + student + "листов бумаги");
 
+        //4
         float machinePerformance = 16 / 2;
         System.out.println("За 20 минут машина произвела" + machinePerformance * 20 + "штук бутылок");
         System.out.println("За сутки машина произвела" + machinePerformance * 60 + "штук бутылок");
         System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 3 + "штук бутылок");
         System.out.println("За 20 минут машина произвела" + machinePerformance * 24 * 60 * 30 + "штук бутылок");
 
+        //5
         byte a3 = 120;
         byte b3 = 2;
         byte c3 = 4;
-        System.out.println("В школе, где'+(a3/(b3+c3)классов нужно" + (a3 / 6 * b3 + "банок белой краски и") + (a3 / 6 * c3
+        int classes = a3 / (b3 + c3);
+        System.out.println("В школе, где" + classes + "классов нужно" + classes * b3 + "банок белой краски и" + classes * c3
                 + "банок коричневой краски");
 
+//6
         double banana = 5 * 0.08;
         double milk = 100 * 0.105 * 2;
         double iceCreamSundae = 2 * 0.100;
         double rawEggs = 4 * 0.070;
-        System.out.println("banana" + "milk" + "iceCreamSundae" + "rawEggs") * 1000);
+        System.out.println((banana + milk + iceCreamSundae + rawEggs) * 1000);
+//7
 
         double a7 = 7.0;
         double b7 = 0.250;
         double c7 = 0.500;
-        System.out.println(a7 / b7 + a7 / c7);
+        System.out.println(a7 / b7 );
+        System.out.println(a7 / c7);
 
-
-        double M = 67760 * 10 / 100;
-        double D = 83690 * 10 / 100;
-        double K = 76230 * 10 / 100;
-        System.out.println("M" + "D" + "K") * 12;
-        System.out.println('67760+83690+76230') * 12;
-        System.out.println('("M"+"D"+"K"') * 12 -(67760+83690+76230)*12;
+//8
+        double M = 67760;
+        double D = 83690;
+        double K = 76230;
+        double sum=M+D+K;
+        System.out.println("до увеличения: " + sum * 12);
+        System.out.println("после увеличения: " + sum * 1.1 * 12);
+        System.out.println("разница: " + sum * 0.1 * 12);
     }
 }
 
